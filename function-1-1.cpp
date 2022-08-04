12a0ee66beca5d8b2c7a395b9a7cdfd9aa7fb387
Kaifeng20 <a1846570@adelaide.edu.au>
2022-08-04 20:10:23 +0800

    first commit

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int diagonal(int array[4][4])
+int sum_diagonal(int array[4][4])
 {
     int sum = 0;
 
     for(int i = 0; i<4; i++)
     {
-    for(int j=0; j<4; j++)
-    {
-    sum += array[i][j];
+        for(int j=0; j<4; j++)
+        {
+        sum += array[i][j];
 
-    }
+        }
     }
     return sum;
 
@@ -19,8 +19,8 @@ int diagonal(int array[4][4])
 
 
 //--------//
-// int main(){
-//     int test[4][4] = {{0,1,2,1},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
+int main(){
+    int array[4][4] = {{0,1,2,1},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
 
-//     cout<< "The sum of the elements along the main diagonal is: " << diagonal(test) << endl << endl;    
-// }
+    cout<< "The sum of the elements along the main diagonal is: " << sum_diagonal(array) << endl << endl;    
+}
