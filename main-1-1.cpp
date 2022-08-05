f6d6d66db21dc98b1972199b99eaa7fecdcf0c74
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-05 11:56:33 +0800

    Add files via upload

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-extern int sum_diagonal(int array[4][4])
+extern int sum_diagonal(int array[4][4]);
 
 int main(){
     int test[4][4] = {{0,1,2,1},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
