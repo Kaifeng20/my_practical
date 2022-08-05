b7f3f2abc05ec6d13274a470121f2556aa06d533
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-05 15:10:42 +0800

    Add files via upload

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -16,10 +16,10 @@ int binary_to_int(int binary_digits[], int number_of_digits)
 
 //----------
 
-int main(){
+// int main(){
     
-    int array[10] = {1,1,0,0,1,0,0,1,0,1}; 
+//     int array[5] = {1,0,0,1,0}; 
     
-    cout <<binary_to_int(array,10)<< endl;
-    return 0;
-}
+//     cout<<binary_to_int(array,5)<<" ";
+//     cout<<endl;
+// }
