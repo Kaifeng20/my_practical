05addc0f98079db6e5a79b5ba627eb05d7b47e2a
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-05 15:41:16 +0800

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -5,7 +5,7 @@ extern int is_identity(int array[10][10]);
 
 int main(){
     int array[10][10] = {
-    {3,0,0,0,0,0,0,0,0,0}, 
+    {1,0,0,0,0,0,0,0,0,0}, 
     {0,1,0,0,0,0,0,0,0,0}, 
     {0,0,1,0,0,0,0,0,0,0}, 
     {0,0,0,1,0,0,0,0,0,0}, 
@@ -17,14 +17,6 @@ int main(){
     {0,0,0,0,0,0,0,0,0,1}, 
     }; 
 
-    if (is_identity(array))
-    {
-        cout<<"This is identity marix"<<endl;
-    }
-    else
-    {
-        cout<<"This is not identity marix"<<endl;
-    }
-
+    std::cout << is_identity(array) << std::endl;
     return 0;
 }
