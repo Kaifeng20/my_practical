f96938fe0842a31b833039b6ffe3fb8a97071de1
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-05 16:45:06 +0800

    Add files via upload

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -62,14 +62,14 @@ void count_digits(int array[4][4]){
 }
 
 
-int main(){
-    int array[4][4] = {
-        {1,2,3,4},
-        {1,2,3,4},
-        {1,2,3,4},
-        {1,2,3,4}
-    };
-    count_digits(array);
-    cout<<endl;
+// int main(){
+//     int array[4][4] = {
+//         {1,2,3,4},
+//         {1,2,3,4},
+//         {1,2,3,4},
+//         {1,2,3,4}
+//     };
+//     count_digits(array);
+//     cout<<endl;
 
-}
+// }
