0cf302d2dbcb68ce6a13f7e739822ebfefca75dd
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-05 16:07:54 +0800

    Add files via upload

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -6,25 +6,14 @@ int is_identity(int array[10][10]){
     {
         for (int j = 0; j < 10; j++)
         {
-            if (i == j)
-            {
-                if (array[i][j] != 1)
-                    return 0;
+            if (j == i && array[i][j] != 1)
+            return 0;
 
-            }
-            else if (i != j)
-            {
-                if (array[i][j] != 0)
-                    return 0;
-
-            }
-
-            else
-            {
-                return 1;
-            }
+            else if(i != j && array[i][j] != 0)
+            return 0;
         }
     }
+    return 0;
 }
 
 
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -18,5 +18,5 @@ int main(){
     }; 
 
     std::cout << is_identity(array) << std::endl;
-    return 0;
+
 }
