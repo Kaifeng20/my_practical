599fe4aee5831a7dc604480c5e4e9cc715ec2676
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-05 16:10:00 +0800

    Add files via upload

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -7,10 +7,10 @@ int is_identity(int array[10][10]){
         for (int j = 0; j < 10; j++)
         {
             if (j == i && array[i][j] != 1)
-            return 0;
+                return 0;
 
             else if(i != j && array[i][j] != 0)
-            return 0;
+                return 0;
         }
     }
     return 0;
