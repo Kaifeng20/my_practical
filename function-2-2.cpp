1ec0c3ed8bb83331a8509670e635453bbfed2292
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-18 21:17:14 +0800

    Add files via upload

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -3,36 +3,26 @@ using namespace std;
 
 int max_sub_sum(int *nums,int length){
     
-    Int maxFar = INT_MIN, maxEnd = 0;
+    int maxFar = INT_MIN;
+    int maxEnd = 0;
     
     for (int i = 0; i < length; i++){
         
-        maxEnd = maxEnd + arr[i];
+        int maxEnd = maxEnd + nums[i];
         if (maxFar < maxEnd)
             maxFar = maxEnd;
         
-        if (maxEnd < 0)
+        else if (maxEnd < 0)
             maxEnd = 0;
         }
     return maxFar;
 }
 
-
-#include<iostream>
-using namespace std;
-
-int max_sub_sum(int *nums,int length){
+int main(){
+    int arr[] = { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int maxSum = max_sub_sum(arr, n);
     
-    int maxFar = INT8_MIN, maxEnd = 0;
-    
-    for (int i = 0; i < length; i++){
-        
-        maxFar = maxEnd + nums[i];
-        if (maxFar < maxEnd)
-            maxFar = maxEnd;
-        
-        if (maxEnd < 0)
-            maxEnd = 0;
-        }
-    return maxFar;
+    cout << maxSum;
+    return 0;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 extern int max_sub_sum(int *nums,int length);
 
 int main(){
-    int arr[] = {31,-41,59,26,-53,58,97,-93,-23,84};
+    int arr[] = {31, -41, 59, 26, -53, 58, 97, -93, -23, 84};
     int n = sizeof(arr)/sizeof(arr[0]);
     int maxSum = max_sub_sum(arr, n);
     
