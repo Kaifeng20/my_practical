e1577691374978ce903d4a22c7734a9b5b68b885
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-18 21:05:01 +0800

    Add files via upload

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -6,7 +6,7 @@ void copy_doubles(double *old_array,double *new_array,int length);
 int main(){
     double arr1[6]={1,2,3,4,5,6};
     double arr2[6];
-    cpyda(arr1,arr2,6);
+    copy_doubles(arr1,arr2,6);
     
     for(int i=0;i<5;i++)
         cout<<*(arr2+i)<<" ";
