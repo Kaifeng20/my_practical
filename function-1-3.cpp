a30c8cdd3005b8efd7ca3b4873f51a50ae7645e4
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-18 21:01:33 +0800

    Add files via upload

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -3,4 +3,8 @@ using namespace std;
 
 void copy_integers(int old_array[],int new_array[],int length){
     
+    for(int i=0; i<length; i++){
+        *(new_array + i) = *(old_array + i);
+        }
+
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,9 +5,9 @@ extern void copy_integers(int old_array[],int new_array[],int length);
 
 int main() {
     
-    int old_array[] = {1,2,3,4,5,6,7,8,9,10};
-    int new_array[10];
-    copy_integers(old_array, new_array, 10);
+    int old_array[8] = {1,2,3,4,5,6,7,8};
+    int new_array[8];
+    copy_integers(old_array, new_array, 8);
     
     for(int i=0; i<10; i++)
         
