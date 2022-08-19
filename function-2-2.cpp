75e27f1d4c3ab3eaa4b8be24170bb68be9d7d416
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-08-19 11:30:55 +0800

    Add files via upload

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,38 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int max_sub_sum(int *nums,int length){
-    
-    Int maxFar = INT_MIN, maxEnd = 0;
-    
-    for (int i = 0; i < length; i++){
-        
-        maxEnd = maxEnd + arr[i];
-        if (maxFar < maxEnd)
-            maxFar = maxEnd;
-        
-        if (maxEnd < 0)
-            maxEnd = 0;
-        }
-    return maxFar;
-}
-
-
-#include<iostream>
-using namespace std;
-
-int max_sub_sum(int *nums,int length){
-    
-    int maxFar = INT8_MIN, maxEnd = 0;
+    int max_so_far = INT8_MIN, max_ending_here = 0;
     
     for (int i = 0; i < length; i++){
-        
-        maxFar = maxEnd + nums[i];
-        if (maxFar < maxEnd)
-            maxFar = maxEnd;
-        
-        if (maxEnd < 0)
-            maxEnd = 0;
+        max_ending_here = max_ending_here + nums[i];
+        if (max_so_far < max_ending_here)
+            
+            max_so_far = max_ending_here;
+        if (max_ending_here < 0)
+            
+            max_ending_here = 0;
         }
-    return maxFar;
+    return max_so_far;
 }
