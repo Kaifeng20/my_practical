dd0af8695edb8f8ad1e8f7432104802f11553982
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-09-02 15:54:17 +0800

    Add files via upload

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
 using namespace std;
+#include <string>
+#include "Capybara.h" 
 
-extern int sum_diagonal(int array[4][4]);
+int main()
+{
+    Capybara c1,c2,c3,c4;
 
-int main(){
-    int test[4][4] = {{0,1,2,1},{2,3,4,5},{3,4,5,6},{4,5,6,7}};
+    cout <<"before setting name: " << c1.getName() <<endl;
 
-    cout<< "The sum of the elements along the main diagonal is: " << sum_diagonal(test) << endl << endl;    
+    c1.setName("test");
 
-    return 0;
+    cout <<"after setting name: " << c1.getName() <<endl;
+
+    cout <<"before setting age: " << c1.getAge() <<endl;
+
+    c1.setAge(23);
+
+    cout <<"after setting age: " << c1.getAge() <<endl;
+
+    cout << "before setting m2 " << c2.getName() <<" " << c2.getAge() << endl;
+
+    c2.setName("test2");
+
+    c2.setAge(22);
+
+    cout << "after setting m2  " << c2.getName() <<" " << c2.getAge() << endl;
+
+    
+
+    cout << "before setting m3  " << c3.getName() <<" " << c3.getAge() << endl;
+
+    c3.setName("test3");
+
+    c3.setAge(21);
+
+    cout << "after setting m3  " << c3.getName() <<" " << c3.getAge() << endl;
+
+    
+
+    cout << "before setting m4  " << c4.getName() <<" " << c4.getAge() << endl;
+
+    c4.setName("test4");
+
+    c4.setAge(32);
+
+    cout << "after setting m4  " << c4.getName() <<" " << c4.getAge() << endl;
+
 }
