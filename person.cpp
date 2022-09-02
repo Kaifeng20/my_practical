b03cb50ba269e75ccb5e693ee04aec94fa83f417
Kaifeng20 <109945703+Kaifeng20@users.noreply.github.com>
2022-09-02 15:50:16 +0800

    Add files via upload

---
+++ person.cpp
@@ -0,0 +1,28 @@
+#include "person.h"
+
+
+Person::Person(int mySalary, string myName)
+{
+    name = myName;
+    salary = mySalary;
+}
+
+void Person::setName(string myname)
+{
+    name = myname;
+}
+
+string Person::getName()
+{
+    return name;
+}
+
+void Person::setSalary(int mySalary)
+{
+    salary = mySalary;
+}
+
+int Person::getSalary()
+{
+    return salary
+}
---
+++ person.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+class Person
+{
+private:
+    int salary;
+    string name;
+
+public:
+
+    Person(int mySalary, string myName); // a name and salary must be provided to create a Person
+    void setName(string myName);         // change the Person's name
+    string getName();
+    void setSalary(int mySalary);        // change the Person's salary
+    int getSalary();
+};
